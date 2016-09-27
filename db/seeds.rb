38ba7895ba7b080332ff2e6f3452f441ba9c17c3
Brian Jennings <brian.j.jennings@gmail.com>
2016-09-27 10:41:19 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,14 @@ create_song(name: 'Maneater',
             url: 'https://p.scdn.co/mp3-preview/85538cf6e2a89e0fe2c85049cff9eece282b7151',
 )
 
-create_song(name: 'Private Eyes',
+create_song(name: "I Can't Go For That",
             artist: 'Hall & Oates',
             album_art: 'https://i.scdn.co/image/7c270ba6ca0991b05079aed0460628857270e7b2',
             url: 'https://p.scdn.co/mp3-preview/4a558e1144aba588135ba366ea5a705a3f653b94',
+)
+
+create_song(name: "CAWWW",
+            artist: 'Metro Boomin',
+            album_art: 'https://yachty.herokuapp.com/crow_pizza.jpg',
+            url: 'https://yachty.herokuapp.com/crow.mp3',
 )
